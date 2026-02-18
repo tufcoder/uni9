55c67114fc9b964baf1e61a7103a9fd37709d702
Oswaldo Castro <oswaldogpc@uni9.edu.br>
2026-02-18 00:08:55 -0300

    refactor: tela de login atualizada

--- primeiro-semestre/projeto-integrado/src/login.php
+++ primeiro-semestre/projeto-integrado/src/login.php
@@ -4,17 +4,122 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Projeto Integrado Uninove</title>
+    <style>
+      html {
+        box-sizing: border-box;
+      }
+
+      *, *::after, *::before {
+        box-sizing: inherit;
+      }
+
+      * {
+        margin: 0;
+        font: inherit;
+      }
+
+      body {
+        font: normal 1rem/1.4 Arial, sans-serif;
+      }
+
+      .login-wrapper {
+        /* width: 50rem;
+        margin: 0 auto; */
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        /* display: grid;
+        place-items: center;
+        min-height: 100vh; */
+      }
+
+      .title {
+        font-weight: bold;
+        font-size: 2.5rem;
+      }
+
+      form {
+        display: flex;
+        flex-direction: column;
+      }
+
+      form button {
+        margin-top: 0.5rem;
+      }
+
+      .radio-wrapper {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+        text-transform: uppercase;
+      }
+
+      .content-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 0.25rem;
+      }
+    </style>
   </head>
   <body>
-    <div id="app">
-      <h1>Projeto Integrado</h1>
+    <div class="login-wrapper">
+      <h1 class="title">Login</h1>
       <form method="POST">
-        <label for="name">Email</label>
-        <input type="text" name="email" id="email" placeholder="exemplo@email.com">
-        <label for="name">Senha</label>
-        <input type="password" name="password" id="password">
-        <button type="submit">Login</button>
+          <div class="radio-wrapper">
+            <label for="radio-ra">
+              <input type="radio" name="id-type" id="radio-ra" checked>
+              <span>ra</span>
+            </label>
+            <label for="radio-cpf">
+              <input type="radio" name="id-type" id="radio-cpf">
+              <span>cpf</span>
+            </label>
+          </div>
+          <div class="content-wrapper">
+            <input type="text" name="input-id" id="input-id" placeholder="Digite seu RA">
+            <input type="password" name="password" id="password" placeholder="Digite sua senha">
+            <button type="submit">Entrar</button>
+          </div>
       </form>
     </div>
+    <script>
+      const radioRA = document.getElementById('radio-ra')
+      const radioCPF = document.getElementById('radio-cpf')
+      const inputId = document.getElementById('input-id')
+
+      window.addEventListener('DOMContentLoaded', function() {
+        if (radioCPF.checked) {
+          inputId.placeholder = 'Digite seu CPF'
+          inputId.name = 'cpf'
+        } else {
+          inputId.placeholder = 'Digite seu RA'
+          inputId.name = 'ra'
+        }
+      })
+
+      radioRA.addEventListener('change', function() {
+        if (radioRA.checked) {
+          inputId.placeholder = 'Digite seu RA'
+          inputId.name = 'ra'
+          inputId.id = 'input-id'
+          inputId.value = ''
+          inputId.focus()
+        }
+      })
+
+      radioCPF.addEventListener('change', function() {
+        if (radioCPF.checked) {
+          inputId.placeholder = 'Digite seu CPF'
+          inputId.name = 'cpf'
+          inputId.id = 'input-id'
+          inputId.value = ''
+          inputId.focus()
+        }
+      })
+    </script>
   </body>
 </html>
--- primeiro-semestre/projeto-integrado/src/scripts/geral.sql
+++ primeiro-semestre/projeto-integrado/src/scripts/geral.sql
@@ -1,12 +1,59 @@
-create table if not exists users (
-  id int not null primary key auto_increment,
-  email varchar(100) not null unique,
-  password varchar(100) not null,
-  username varchar(100) null,
-  created_at datetime not null default CURRENT_TIMESTAMP,
-  updated_at datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS roles (
+	id int NOT NULL PRIMARY KEY auto_increment,
+	role varchar(50) NOT NULL UNIQUE,
+	created_at datetime not null default CURRENT_TIMESTAMP,
+	updated_at datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 );
 
-insert into users (email, password) values ('johndoe@email.com', '1234');
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY auto_increment,
+	cpf varchar(11) not null unique,
+	password varchar(100) not null,
+	created_at datetime not null default CURRENT_TIMESTAMP,
+	updated_at datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
+);
 
-select * from users;
+CREATE TABLE IF NOT EXISTS user_roles (
+	user_id int NOT NULL,
+	role_id int NOT NULL,
+	created_at datetime not null default CURRENT_TIMESTAMP,
+	updated_at datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+	PRIMARY KEY (user_id, role_id),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS students (
+	user_id int NOT NULL,
+	ra varchar(15) NOT NULL UNIQUE,
+	created_at datetime not null default CURRENT_TIMESTAMP,
+	updated_at datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+	PRIMARY KEY (user_id, ra),
+	FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO roles (role) values ('Admin');
+INSERT INTO roles (role) values ('Aluno');
+INSERT INTO roles (role) values ('Coordenador');
+INSERT INTO roles (role) values ('Professor');
+
+INSERT INTO users (cpf, password) values ('12345678909', '1234');
+INSERT INTO users (cpf, password) values ('09876543211', '1234');
+INSERT INTO users (cpf, password) values ('23456789010', '1234');
+INSERT INTO users (cpf, password) values ('34567890122', '1234');
+
+INSERT INTO user_roles (user_id, role_id) values (1, 1);
+INSERT INTO user_roles (user_id, role_id) values (2, 2);
+INSERT INTO user_roles (user_id, role_id) values (3, 3);
+INSERT INTO user_roles (user_id, role_id) values (4, 4);
+INSERT INTO user_roles (user_id, role_id) values (4, 3);
+
+INSERT INTO students (user_id, ra) values (2, '1234567890');
+
+SELECT
+	r.role
+	,u.*
+FROM users u
+	INNER JOIN user_roles ur ON u.id = ur.user_id
+	INNER JOIN roles r ON ur.role_id = r.id;
